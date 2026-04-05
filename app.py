fe7a7dea5a39bc69d5073ab81946acc7b8766cd8
slugbug432-sketch <slugbug432@gmail.com>
2026-04-05 17:52:06 -0500

    Fix Render deployment

--- app.py
+++ app.py
@@ -466,17 +466,17 @@ def add_supplier():
 # Main
 # --------------------------
 
+import os
+
 if __name__ == "__main__":
 
     with app.app_context():
-
         db.create_all()
         create_default_users()
-    import os
 
-port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
 
-app.run(
-    host="0.0.0.0",
-    port=port
-)
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
